3c911dfdaa781b71d3a808b936edb601c771e533
Sachin Singh <ssingh@initializ.io>
2025-01-05 23:37:54 +0530

    Implemented google maps

--- src/app/components/ind-map/ind-map.component.ts
+++ src/app/components/ind-map/ind-map.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { GoogleMapsModule } from "@angular/google-maps";
 
 @Component({
-  selector: 'app-ind-map',
+  selector: "app-ind-map",
   standalone: true,
-  imports: [],
-  templateUrl: './ind-map.component.html',
-  styleUrl: './ind-map.component.css'
+  imports: [GoogleMapsModule],
+  templateUrl: "./ind-map.component.html",
+  styleUrl: "./ind-map.component.css",
 })
 export class IndMapComponent {
+  center: google.maps.LatLngLiteral = { lat: 28.5961, lng: 77.1587 };
+  zoom = 8;
+  markerPositions: google.maps.LatLngLiteral[] = [
+    { lat: 28.5961, lng: 77.1587 },
+  ];
 
+  addMarker(event: google.maps.MapMouseEvent) {
+    console.log(event);
+
+    if (event.latLng) {
+      this.markerPositions.push(event.latLng.toJSON());
+    }
+  }
 }
