4d167b16a74b09612369802f64a6203c33a040d7
sidhant pandey <sidhantpandeynp6@gmail.com>
2025-01-08 20:15:22 +0530

    * Change add onclick event on launch drone button

--- src/app/components/mission-control/mission-control.component.ts
+++ src/app/components/mission-control/mission-control.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mission-control',
@@ -8,4 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './mission-control.component.css'
 })
 export class MissionControlComponent {
+  constructor(private router : Router){}
+  nevigateToLaunchDrone(){
+    this.router.navigate(["/drone-feed"]);
+  }
 }
