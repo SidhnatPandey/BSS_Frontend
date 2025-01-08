8dbf2ca7401580602ef56da6d8a2a3a6f4a2747b
Sachin Singh <ssingh@initializ.io>
2025-01-08 15:55:10 +0530

    created Drone design on the left pannel

--- src/app/components/drone/drone-layout/drone-layout.component.ts
+++ src/app/components/drone/drone-layout/drone-layout.component.ts
@@ -19,36 +19,19 @@ import { SidebarComponent } from "../../../Sidebar/sidebar.component";
   styleUrl: "./drone-layout.component.css",
 })
 export class DroneLayoutComponent {
-  tiles = [
-    { text: "One", cols: 3, rows: 1, color: "lightblue" },
-    { text: "Two", cols: 1, rows: 2, color: "lightgreen" },
-    { text: "Three", cols: 1, rows: 1, color: "lightpink" },
-    { text: "Four", cols: 2, rows: 1, color: "#DDBDF1" },
+  drones = [
+    { name: "One", isOnline: true },
+    { name: "Two", isOnline: false },
+    { name: "Three", isOnline: true },
+    { name: "Four", isOnline: false },
   ];
-  leftItems = ["Main Entrance", "Parking Lot", "Loading Dock", "Backyard"];
-  selectedSensor: { name: string; data: string } | null = null;
+  selectedDrone: any;
   videoStreamUrl: string = "http://127.0.0.1:8000/api/video_feed";
-  videoStreamUrlRajak: string = "http://127.0.0.1:8000/api/ptz_video_feed";
-  videoStreamUrlPtz: string = "http://127.0.0.1:8000/api/ptz_video_feed";
-  videoStreamUrlHsti: string = "http://127.0.0.1:8000/api/thermal_video_feed";
-  videoStreamUrlLorros: string = "http://127.0.0.1:8000/api/ptz_video_feed";
 
   ngOnInit(): void {
-    // Pre-select PTZ sensor as the default
-    this.selectSensor({ name: "PTZ", data: "Live data from PTZ" });
+    this.selectDrone({ ...this.drones[1] });
   }
-  selectSensor(sensor: { name: string; data: string }) {
-    this.selectedSensor = sensor;
-    if (sensor.name === "RAJAK") {
-      this.videoStreamUrl = this.videoStreamUrlRajak;
-    } else if (sensor.name === "PTZ") {
-      this.videoStreamUrl = this.videoStreamUrlPtz;
-    } else if (sensor.name === "HSTI") {
-      this.videoStreamUrl = this.videoStreamUrlHsti;
-    } else if (sensor.name === "LORROS") {
-      this.videoStreamUrl = this.videoStreamUrlLorros;
-    } else {
-      this.videoStreamUrl = this.videoStreamUrlPtz;
-    }
+  selectDrone(drone: any) {
+    this.selectedDrone = drone;
   }
 }
