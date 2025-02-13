3e830c4acb42c38fa04134d063d0a20e4d405b8d
sidhant pandey <sidhantpandeynp6@gmail.com>
2025-02-13 23:00:06 +0530

    * Resolve build issue * Add the meta data API

--- src/app/aetos/components/meta-data/meta-data.component.ts
+++ src/app/aetos/components/meta-data/meta-data.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { SseClient } from 'ngx-sse-client';
 
 @Component({
   selector: 'app-meta-data',
@@ -9,16 +11,33 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './meta-data.component.html',
   styleUrl: './meta-data.component.css'
 })
-export class MetaDataComponent {
+export class MetaDataComponent implements OnInit {
 
   currentTime: string | undefined;
   oneMinuteBefore: string | undefined;
+  private apiUrl = "http://127.0.0.1:1000/api/metadata";
+  metadata: any = [];
 
-  constructor() {
+  constructor(private sseClient : SseClient) {
     const now = new Date();
     this.currentTime = now.toISOString(); // Current time in ISO format
 
     const oneMinuteAgo = new Date(now.getTime() - 60 * 1000); // Subtract 1 minute (60 seconds * 1000 milliseconds)
     this.oneMinuteBefore = oneMinuteAgo.toISOString();
   }
+
+   ngOnInit(): void {
+      // Fetch notifications through sse events
+      const headers = new HttpHeaders();
+      this.sseClient.stream(this.apiUrl, { keepAlive: true, reconnectionDelay: 1_000, responseType: 'event' }, { headers }, 'GET').subscribe((event) => {
+        if (event.type === 'error') {
+          const errorEvent = event as ErrorEvent;
+          console.error(errorEvent.error, errorEvent.message);
+        } else {
+          const messageEvent = event as MessageEvent;
+          this.metadata = messageEvent.data;
+          console.info(`SSE request with type "${messageEvent.type}" and data "${messageEvent.data}"`);
+        }
+      });
+    }
 }
