6056a6ecf8a858098a70e88a5a5d4d2311ba655b
sidhant pandey <sidhantpandeynp6@gmail.com>
2025-02-24 15:24:17 +0530

    *feature add bss-form in the main screen

--- src/app/aetos/aetos-dashboard/aetos-dashboard.component.ts
+++ src/app/aetos/aetos-dashboard/aetos-dashboard.component.ts
@@ -84,12 +84,14 @@
 //   }
 // }
 
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { Router } from "@angular/router";
+import bootstrap, {Modal} from 'bootstrap';
  
 interface BSS {
   id: number;
@@ -102,11 +104,11 @@ interface BSS {
 @Component({
   selector: "app-aetos-dashboard",
   standalone: true,
-  imports: [FontAwesomeModule, CommonModule, MatCardModule, MatIconModule],
+  imports: [FontAwesomeModule, CommonModule, MatCardModule, MatIconModule, FormsModule, ReactiveFormsModule],
   templateUrl: "./aetos-dashboard.component.html",
   styleUrl: "./aetos-dashboard.component.css",
 })
-export class AetosDashboardComponent {
+export class AetosDashboardComponent implements OnInit {  
   photos = [
     {
       label: "BSS 1",
@@ -193,9 +195,18 @@ export class AetosDashboardComponent {
     },
   ];
  
-  constructor(private router: Router) {}
+  @ViewChild('closeBtn') closeBtn!: ElementRef<HTMLButtonElement>;
+  
+  constructor(private fb: FormBuilder, private router: Router) {}
+  bssForm!: FormGroup;
  
   ngOnInit(): void {
+    this.bssForm = this.fb.group({
+      bssName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9 ]+$')],],
+      bssLatitude: ['', [Validators.required, Validators.pattern('^-?\\d+(\\.\\d+)?$')],],
+      bssLongitude: ['', [Validators.required, Validators.pattern('^-?\\d+(\\.\\d+)?$')],],
+      bssImage: [null, Validators.required],
+    });
     this.startRefreshTimer();
     // Delay the alert by 10 seconds (10000 milliseconds)
     setTimeout(() => {
@@ -203,6 +214,31 @@ export class AetosDashboardComponent {
       this.alertCount++;
     }, 5000);
   }
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file && file.type.startsWith('image/')) {
+      this.bssForm.patchValue({ bssImage: file });
+    } else {
+      this.bssForm.patchValue({ bssImage: null });
+    }
+  }
+
+  onSubmit() {
+    if (this.bssForm?.valid) {   
+      this.locations.push({
+        id: this.locations.length + 1,
+        name: this.bssForm?.value?.bssName,
+        status: "active",
+        lastUpdate: 0,
+        image: "assets/bss/b4.png",
+      });
+      this.closeBtn.nativeElement.click(); 
+      this.bssForm.reset();
+    } else {
+      console.log('Form is invalid');
+    }
+  };
  
   startRefreshTimer(): void {
     setInterval(() => {
@@ -218,7 +254,7 @@ export class AetosDashboardComponent {
           photo.lastUpdate = 0; // Reset after 15 seconds
         }
       });
-    }, 1000); // Increment every second
+    }, 1000);
   }
  
   navigateToLiveFeed(photo: any): void {
@@ -227,4 +263,9 @@ export class AetosDashboardComponent {
       queryParams: { location: photo.label },
     });
   }
+
+  handleButtonClick() {
+    console.log('Floating button clicked');
+  }
+
 }
